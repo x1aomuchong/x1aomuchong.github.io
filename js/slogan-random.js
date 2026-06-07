9c8f12ad6a8f518d3cbe3b5db25de93878c02fb7
MuChen Xiao <x1aomuchong@outlook.com>
2026-06-07 21:40:41 +0800

    Site updated: 2026-06-07 21:40:37

---
+++ js/slogan-random.js
@@ -0,0 +1,25 @@
+/**
+ * 首页格言随机池
+ * 每次访问主页时随机显示其中一句格言
+ *
+ * 修改方式：直接编辑下方 mottoes 数组，增删改即可
+ */
+(function () {
+  var subtitle = document.getElementById('subtitle');
+  if (!subtitle) return;
+
+  var mottoes = [
+    'Hypotheses non fingo',
+    'Amor est vitae essentia',
+    'Qui non proficit deficit',
+    'Esto quod audes',
+    'Homo plus est quam humanitas',
+    'Ipsa scientia potestas est',
+    'Veritas vos liberabit',
+    'Vox populi, vox Dei',
+  ];
+
+  var pick = mottoes[Math.floor(Math.random() * mottoes.length)];
+  subtitle.setAttribute('data-typed-text', pick);
+  subtitle.textContent = pick;
+})();
